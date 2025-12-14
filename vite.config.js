bdd355bcebb6aa27a149a62b0d09bc1e36645007
Pavani Obul Reddy <obulreddypavani90@gmail.com>
2025-12-14 09:08:55 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -15,11 +15,11 @@
 // })
 
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Task-Deploy/"
-});
+})
 
